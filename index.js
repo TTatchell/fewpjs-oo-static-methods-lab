95f70ef8d22d15d2a45c6c07d9bfe393722246fb
TTatchell <tim.tatchell24@gmail.com>
2021-04-09 08:37:10 +1000

    upload but not finished

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    let returnString = '';
+    const arrOfString = [...string];
+    for (let index = 0; index < arrOfString.length; index++) {
+      if (arrOfString[index] !== /^[0-9A-Za-z\s\-]+$/) {
+        returnString = returnString.concat(arrOfString[index])
+      }
+      
+
+
+    }
+    console.log(returnString)
+    return returnString;
+    
+  }
 }
+
+
+console.log(Formatter.sanitize("Hi$%^ Tim!"))
