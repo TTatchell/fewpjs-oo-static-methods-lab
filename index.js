2cb080c109a6e881a9afb0263bd8b2bddf15d740
TTatchell <tim.tatchell24@gmail.com>
2021-04-09 10:55:40 +1000

    progress

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const charCheck = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v,","w","x","y","z","0","1","2","3","4","5","6","7","8","9","0","-","'"," "]
+const conjWords = ["a", "an", "but", "of", "and", "for", "at", "by", "from"]
 
 class Formatter {
-  constructor () {
-    this.charCheck = charCheck;
-  }
   
   static capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -11,29 +9,55 @@ class Formatter {
 
   static sanitize(string) {
     const arrOfString = [...string];
-    console.log(arrOfString)
+    //console.log(arrOfString)
+    let returnString = '';
     
     for (let index = 0; index < arrOfString.length; index++) {
       for (let i = 0; i < charCheck.length; i++) {
-        if (arrOfString[index] === charCheck[i]){
-          console.log (`${arrOfString[index]} is equal to ${charCheck[i]}`)
+        if (arrOfString[index].toLowerCase() === charCheck[i]){
+          //console.log (`${arrOfString[index]} is equal to ${charCheck[i]}`)
+          returnString = returnString + arrOfString[index];
+          break;
+
         }
-        else console.log("char not found")
         
       }
-      
-
-
-      console.log(arrOfString[index])
-      
-    }
-      
-
-
+            
     }
     //console.log(returnString)
-    //return returnString;
-    
-  }
+    return returnString;
+    } 
 
-console.log(Formatter.sanitize("Hi$%^ Tim!"))
+  static titleize(string) {
+    const arrOfString = string.split(" ");
+    let returnString = '';
+
+    for (let index = 0; index < arrOfString.length; index++) {
+
+      console.log(index)
+      if (index === 0){
+        returnString = returnString + arrOfString[index].toUpperCase();
+      }
+
+
+      for (let i = 0; i < conjWords.length; i++) {
+        if (arrOfString[index].toLowerCase() === conjWords[i]){
+
+          //maybe use splice to remove the bad words from the array???
+
+          returnString = returnString + arrOfString[index];
+          break; 
+      }
+      
+    }
+
+    
+
+
+  }
+  console.log(returnString);
+  return returnString;
+  }
+}
+
+  Formatter.titleize("Hello and my for name at is Tim")
